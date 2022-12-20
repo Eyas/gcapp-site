b023a870ed53f42029cd5eb1d13c2b64a334081c
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2022-12-19 20:00:39 -0500

    Update App Store link.

--- src/download.tsx
+++ src/download.tsx
@@ -5,7 +5,7 @@ import styles from "./download.module.scss";
 
 const ANDROID_URL =
   "https://play.google.com/store/apps/details?id=org.stanthonyhall.gcapp";
-const IOS_URL = "https://apple.com;";
+const IOS_URL = "https://apps.apple.com/us/app/gc-2023/id6444782168;";
 
 export function Download() {
   const os = usePhoneType();
@@ -29,10 +29,10 @@ export function Download() {
             src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
           />
         </a>
-        <a href="https://apps.apple.com/us/app/gc-restorative-dentistry/id424476132?itsct=apps_box_badge&amp;itscg=30200">
+        <a href="https://apps.apple.com/us/app/gc-2023/id6444782168?itsct=apps_box_badge&amp;itscg=30200">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1300060800?h=2013dc29c931acceb82f3116a4a9d0b1"
+            src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1671408000?h=362d2afa5785ec4e0c689ab9a51aea69"
             alt="Download on the App Store"
             style={{ borderRadius: "13px", width: "250px", height: "83px" }}
           />
